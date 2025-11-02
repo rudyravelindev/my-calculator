ef5b890ea97bb824a573d2b3bb3050cca9f3ebbb
rudyravelindev <rudyravelindev@gmail.com>
2025-11-01 21:14:16 -0700

    adding all the remaining pieces for the calculator

--- script.js
+++ script.js
@@ -33,6 +33,8 @@ console.log(division(14, 2));
 let firstNumber;
 let secondNumber;
 let operator;
+let displayValue = '0';
+let waitingForSecondNumber = false;
 
 // Operation
 function operate(operator, a, b) {
@@ -49,9 +51,111 @@ function operate(operator, a, b) {
     return division(a, b);
   } else return 'Please choose an operator';
 }
+// for testing
+// console.log(operate('/', 10, 0));
+// console.log(operate('-', 10, 4));
+// console.log(operate('$', 2, 3));
+// console.log(operate('+', 5, 3));
+// console.log(operate('/', 10, 0));
+// console.log(operate('+', 5, 3));
+// console.log(operate('/', 10, 0));
+// console.log(operate('*', 7, 6));
 
-console.log(operate('/', 10, 0)); // error message
-console.log(operate('-', 10, 4)); // Should be 6
-console.log(operate('$', 2, 3)); // Please choose an operator
-console.log(operate('+', 5, 3)); // Should be 8
-console.log(operate('/', 10, 0)); // error message
+const display = document.getElementById('display');
+
+const numberButtons = document.querySelectorAll('[data-type="number"]');
+const operatorButtons = document.querySelectorAll('[data-type="operator"]');
+const functionButtons = document.querySelectorAll('[data-type="function"]');
+
+numberButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    inputNumber(button.dataset.value);
+  });
+});
+function inputNumber(number) {
+  if (displayValue === '0' || waitingForSecondNumber) {
+    displayValue = number;
+    waitingForSecondNumber = false;
+  } else {
+    displayValue += number;
+  }
+
+  display.value = displayValue;
+}
+const clearButton = document.querySelector('[data-action="clear"]');
+
+clearButton.addEventListener('click', clearCalculator);
+
+function clearCalculator() {
+  displayValue = '0';
+  firstNumber = undefined;
+  secondNumber = undefined;
+  operator = undefined;
+  waitingForSecondNumber = false;
+
+  display.value = displayValue;
+}
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    handleOperator(button.dataset.value);
+  });
+});
+function handleOperator(nextOperator) {
+  const inputValue = parseFloat(displayValue);
+
+  if (operator && waitingForSecondNumber) {
+    operator = nextOperator;
+    return;
+  }
+
+  if (firstNumber === undefined) {
+    firstNumber = inputValue;
+  } else if (operator) {
+    const result = operate(operator, firstNumber, inputValue);
+    displayValue = `${result}`;
+    display.value = displayValue;
+    firstNumber = result;
+  }
+
+  waitingForSecondNumber = true;
+  operator = nextOperator;
+}
+const equalsButton = document.querySelector('[data-action="equal"]');
+
+equalsButton.addEventListener('click', () => {
+  handleEquals();
+});
+
+function handleEquals() {
+  if (
+    firstNumber === undefined ||
+    operator === undefined ||
+    waitingForSecondNumber
+  ) {
+    return;
+  }
+
+  const inputValue = parseFloat(displayValue);
+  const result = operate(operator, firstNumber, inputValue);
+
+  displayValue = `${result}`;
+  display.value = displayValue;
+
+  firstNumber = result;
+  waitingForSecondNumber = true;
+}
+const decimalButton = document.querySelector('[data-action="point"]');
+
+decimalButton.addEventListener('click', () => {
+  inputDecimal();
+});
+
+function inputDecimal() {
+  if (waitingForSecondNumber) {
+    displayValue = '0.';
+    waitingForSecondNumber = false;
+  } else if (!displayValue.includes('.')) {
+    displayValue += '.';
+  }
+  display.value = displayValue;
+}
